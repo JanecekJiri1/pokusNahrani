59423bda806dc372b87dc4b127e0fbbd5aed1a19
Jirka1993 <kleric13@seznam.cz>
2022-01-09 12:08:24 +0100

    mozna to jde

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { FaGithub, FaFacebookF } from "react-icons/fa";
 
 function App() {
   return (
@@ -7,6 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>V prdeli 2</p>
+        <FaGithub /> work?
+        <FaFacebookF /> work?
         <a>Learn React</a>
       </header>
     </div>
